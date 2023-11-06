3ff4659a0c60e12ceaf0299bd904e3de53796a84
orukahairuka <sakuraierika03301109@gmail.com>
2023-11-06 19:34:40 +0900

    firebaseの環境構築

--- lib/main_model.dart
+++ lib/main_model.dart
@@ -1,14 +1,32 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:uuid/uuid.dart';
+import 'package:uuid/rng.dart';
+import 'package:uuid/data.dart';
 
 //viewとmodelの橋渡しする
 final mainProvider = ChangeNotifierProvider(((ref) => MainModel()));
+final uuid = Uuid();
 
 class MainModel extends ChangeNotifier {
   int counter = 0;
 
-  void incrementCounter() {
+  Future<void> createUser({required BuildContext context}) async {
     counter++;
+    final v4 = uuid.v4();
+    final Map<String, dynamic> userData = {
+      'userName': 'Alice',
+      'uid': v4,
+    };
+    //awaitで順次実行する
+    await FirebaseFirestore.instance
+        .collection('users')
+        .doc('firstUser')
+        .set(userData);
+    //処理が終わったことを知らせるメッセージ表示する
+    ScaffoldMessenger.of(context)
+        .showSnackBar(SnackBar(content: Text('ユーザーが作成できました')));
     notifyListeners();
   }
 }
